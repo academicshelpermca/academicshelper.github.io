1c915d8d51ef7919fff5301d317e94b1399ceb52
Academics Helper <academicshelpermca@gmail.com>
2021-05-20 20:37:01 +0530

    1st commit

---
+++ js/alert.js
@@ -0,0 +1,6 @@
+function pna(){
+    alert(" NOT AVAILABLE !! \n\n This subject is theoretical so there is no practical available.");
+}
+function qpna(){
+    alert(" NOT AVAILABLE !! \n\n This subject is purely lab based so there is no mid sem and end sem question paper available.");
+}
